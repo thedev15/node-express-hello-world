d4a5326a39ee3deeab1a7b1f2dda241762406331
thedev15 <caolong518@outlook.com>
2023-08-11 06:46:57 -0700

    Push updates

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var express=require('express');
-var app=express();
+var express = require("express");
+var app = express();
 
-var routes=require('./routes/route.js');
+var routes = require("./routes/route.js");
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/',routes.home);
+app.get("/", routes.home);
 
-app.get('/health', (req, res) => {
-  res.json({ message: 'okay' }).status(200);
+app.get("/health", (req, res) => {
+  res.json({ message: "okay, updated from codedeploy" }).status(200);
 });
 
 var port = process.env.PORT || 3000;
 
-var server=app.listen(port, '0.0.0.0', function(req,res){
-    console.log("Catch the action at http://localhost:"+port);
+var server = app.listen(port, "0.0.0.0", function (req, res) {
+  console.log("Catch the action at http://localhost:" + port);
 });
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /home/ubuntu/node-express-hello-world
-git config --global --add safe.directory /home/ubuntu/node-express-hello-world
 git pull origin master
 pm2 reload
